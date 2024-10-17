8ba09dc70cd2f9be9e74950a1b8f511cba148083
Prado Sandra <sandra.prado@tsoftlatam.com>
2024-10-16 23:18:34 -0300

    Se crea la clase para las funciones compareTo y compareToIgnoreCase

---
+++ src/metodosString/funcionCompareTo.java
@@ -0,0 +1,16 @@
+package metodosString;
+
+public class funcionCompareTo { //Devuelve un entero menor que cero si el String es alfabéticamente menor que
+    // la dada como argumento, cero si los dos String son léxicamente iguales y un entero mayor que cero
+    // si el String es mayor.
+
+    public static void main(String[] args) {
+        String te1= "Hola";
+        String te2= "hola";
+        if(te1.compareToIgnoreCase(te2) == 0)
+            System.out.println("los textos son iguales");
+        else
+            System.out.println("los textos son distintos");
+
+    }
+}
