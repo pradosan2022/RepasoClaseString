5c79241aaec713db9fde324610476813c7965643
Prado Sandra <sandra.prado@tsoftlatam.com>
2024-10-16 23:34:28 -0300

    Se crean las clases para las funciones toLowerCase y ToUpperCase

---
+++ src/metodosString/funcionToLowerCase.java
@@ -0,0 +1,11 @@
+package metodosString;
+
+public class funcionToLowerCase { //Convierte el String a minúsculas
+    public static void main(String[] args) {
+        String t = "Taller de Programación I";
+        System.out.println(t);
+        t = t.toLowerCase();
+        System.out.println("Ahora todo en minúscula: "+t);
+
+    }
+}
---
+++ src/metodosString/funcionToUpperCase.java
@@ -0,0 +1,11 @@
+package metodosString;
+
+public class funcionToUpperCase { //Convierte el String  a mayúsculas
+    public static void main(String[] args) {
+        String t = "taller de Programación i";
+        System.out.println(t);
+        t = t.toUpperCase();
+        System.out.println("Ahora en mayúscula: "+t);
+
+    }
+}
