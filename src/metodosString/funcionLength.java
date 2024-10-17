62cfaec7adc955c11d284460020c0f3c7c90925d
Prado Sandra <sandra.prado@tsoftlatam.com>
2024-10-16 22:43:33 -0300

    Se crea la clase para la funcion Length

---
+++ src/metodosString/funcionLength.java
@@ -0,0 +1,10 @@
+package metodosString;
+
+public class funcionLength { //sirve para saber la cantidad de caracteres que tiene una cadena de texto
+    public static void main(String[] args) {
+        String texto= "Todos quedaremos expertos en java";
+        int x=texto.length();
+        System.out.println("La frase \""+texto+"\" tiene " +x +" caracteres");
+
+    }
+}
