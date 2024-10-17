5e3118632d8387da1818e76ebdec7f0d903a6786
Prado Sandra <sandra.prado@tsoftlatam.com>
2024-10-16 22:34:15 -0300

    -

---
+++ src/metodosString/StringObjects.java
@@ -0,0 +1,14 @@
+package metodosString;
+
+public class StringObjects {
+    public static void main(String[] args) {
+        String cad1="abcde";
+        String cad2 = new String("Hola");//crear un string usando el constructor
+        String cad3 = new String(cad1);
+        String nom = null;
+        String x="";
+
+        System.out.println(cad1);
+
+    }
+}
