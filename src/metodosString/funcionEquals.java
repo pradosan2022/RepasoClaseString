933940fc174f9701115723f7fe8286702f21444f
Prado Sandra <sandra.prado@tsoftlatam.com>
2024-10-16 23:01:08 -0300

    Se crea la clase para la funcion Equals

---
+++ src/metodosString/funcionEquals.java
@@ -0,0 +1,13 @@
+package metodosString;
+
+public class funcionEquals {
+    public static void main(String[] args) {//sirve para comprobar si una cadena de texto es igual a otra
+        String te1= "Hola";
+        String te2= "hola";
+        if(te1.equals(te2))
+            System.out.println("los textos son iguales");
+        else
+            System.out.println("los textos son distintos");
+
+    }
+}
